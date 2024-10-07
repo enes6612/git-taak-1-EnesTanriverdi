6c03afa48e59bc85f39d396212cd65ea2fbf49a7
Enes Tanriverdi <Enes.Tanriverdi@student.pxl.be>
2024-10-07 22:11:55 +0200

    Double click

--- git-taak1/MainWindow.xaml.cs
+++ git-taak1/MainWindow.xaml.cs
@@ -20,5 +20,42 @@ namespace git_taak1
         {
             InitializeComponent();
         }
+
+        private void duneImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string duneTitle = "Dune: Part Two";
+            string duneText = "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.";
+
+            if (e.ClickCount == 2)
+            {
+                resultTextBlock.Text = $@"Title: {duneTitle}
+Short discription: {duneText}";
+            }
+
+        }
+        private void substanceImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string substanceTitle = "The Substance";
+            string substanceText = "A fading celebrity decides to use a black-market drug, a cell-replicating substance that temporarily creates a younger, better version of herself.";
+
+            if (e.ClickCount == 2)
+            {
+                resultTextBlock.Text = $@"Title: {substanceTitle}
+Short discription: {substanceText}";
+            }
+        }
+
+        private void furiosaImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string furiosaTitle = "Furiosa: A Mad Max Saga";
+            string furiosaText = "The origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.";
+
+
+            if (e.ClickCount == 2)
+            {
+                resultTextBlock.Text = $@"Title: {furiosaTitle}
+Short discription: {furiosaText}";
+            }
+        }
     }
 }
